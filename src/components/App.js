1141d45ae2d3389847cf458902dcf0b649d74177
Kevin DA-RE <dare.kevin21000@gmail.com>
2023-05-26 23:43:18 +0200

    Chapitre - Gagnez en temps et en efficacité grâce aux listes et aux conditions-Contextualisez le contenu de vos composants

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/index.css';
+import Banner from './Banner';
+import ShoppingList from './ShoppingList';
+
+function App() {
+  return (
+    <div>
+        <Banner />
+        <ShoppingList />
+    </div>
+        
+  );
+}
+
+export default App;
