a63ebb2ff6818e40bcbdbfb9eb518db0a1da0b0e
Kevin DA-RE <dare.kevin21000@gmail.com>
2023-06-27 23:16:42 +0200

    Chapitre - Partagez votre state entre différents composants

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react'
 import Banner from './Banner'
 import Cart from './Cart'
+import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 
 function App() {
+	const [cart, updateCart] = useState([])
+
 	return (
 		<div>
 			<Banner />
-			<Cart />
-			<ShoppingList />
+			<div className='lmj-layout-inner'>
+			<Cart cart={cart} updateCart={updateCart}  />
+			<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />		
 		</div>
 	)
 }
